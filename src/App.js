16b5dda84924fdd35ad3f3f7d3384c0e32255c1d
Ysquid <ahk648@gmail.com>
2023-01-14 09:49:30 -0600

    adjusted router structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/footer/Footer.js'
 import Nav from './components/nav/Nav.js'
+import Error from './components/error/Error.js'
+import Home from './components/home/Home.js'
+import About from './components/about/About.js'
+import Product from './components/product/Product.js'
+import Shop from './components/locations/Shop.js'
+import Contact from './components/contact/Contact.js'
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>MRK CBD 2.0</h1>
-      <Footer />
-    </div>
+    <Router>
+      <Nav />
+
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/product' element={<Product/>}></Route>
+        <Route path='/shop' element={<Shop/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -7,35 +7,30 @@ import About from './components/about/About.js'
 import Product from './components/product/Product.js'
 import Shop from './components/locations/Shop.js'
 import Footer from './components/footer/Footer.js'
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <Error />
-  },
-  {
-    path: 'about',
-    element: <About />
-  },
-  {
-    path: 'product',
-    element: <Product />
-  },
-  {
-    path: 'shop',
-    element: <Shop />
-  }
-]);
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     errorElement: <Error />
+//   },
+//   {
+//     path: 'about',
+//     element: <About />
+//   },
+//   {
+//     path: 'product',
+//     element: <Product />
+//   },
+//   {
+//     path: 'shop',
+//     element: <Shop />
+//   }
+// ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
