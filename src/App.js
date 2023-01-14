5f002412aec712636fcf397a437a39bdd741b180
Ysquid <ahk648@gmail.com>
2023-01-14 09:39:25 -0600

    basic routing setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import React from 'react';
+import Footer from './components/footer/Footer.js'
+import Nav from './components/nav/Nav.js'
+
 
 function App() {
   return (
     <div className="App">
       <h1>MRK CBD 2.0</h1>
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Error from './components/error/Error.js'
+import About from './components/about/About.js'
+import Product from './components/product/Product.js'
+import Shop from './components/locations/Shop.js'
+import Footer from './components/footer/Footer.js'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />
+  },
+  {
+    path: 'about',
+    element: <About />
+  },
+  {
+    path: 'product',
+    element: <Product />
+  },
+  {
+    path: 'shop',
+    element: <Shop />
+  }
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-
