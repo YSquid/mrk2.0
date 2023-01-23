d4a9737980593c9df7deeb5078328c68d24cb0ea
Ysquid <ahk648@gmail.com>
2023-01-23 16:00:37 -0600

    assets in. start of products page

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./Product.css";
+import Bottle from "../../assets/cbd-bottle.png";
+import USA from '../../assets/USA-NoBG.png'
+import GMP from '../../assets/GMP-NoBG.png'
+import NonGMO from '../../assets/Non-GMO-NoBG.png'
 
 function Product() {
   return (
-    <div>Product</div>
-  )
+    <section className="Product__container">
+      <div className="Product">
+        <div className="Product__left">
+          <div className="bottle">
+          <img src={Bottle} className="bottleimg" />
+          </div>
+          <div className="logos">
+            <img src={USA} />
+            <img src={GMP} />
+            <img src={NonGMO} />
+          </div>
+        </div>
+        <div className="Product__details"></div>
+      </div>
+    </section>
+  );
 }
 
-export default Product
+export default Product;
