2ea0c1a37f1fa2a089b3dee99af238ccaf98e4d8
Ysquid <ahk648@gmail.com>
2023-01-25 12:12:25 -0600

    product page formatted all screen sizes

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -9,17 +9,13 @@ function Product() {
   return (
     <section className="Product__container">
       <div className="Product">
-        <div className="Product__left">
+        <div className="Product__details">
+
           <div className="bottle">
           <img src={Bottle} className="bottleimg" />
           </div>
-          <div className="logos">
-            <img src={USA} />
-            <img src={GMP} />
-            <img src={NonGMO} />
-          </div>
-        </div>
-        <div className="Product__details">
+
+          <div className="description">
           <h2>Full Sprectrum CBD Tincture</h2>
           <h3>900mg CBD in 30mL</h3>
           <h3>Ingredients:</h3>
@@ -27,6 +23,16 @@ function Product() {
             <li>- Fractionated Coconut Oil (MCT Medium Chain Triglycerides)</li>
             <li>- CO2 Extracted Full Spectrum Hemp Oil</li>
           </ul>
+          </div>
+
+        </div>
+
+        <div className="Product__logos">
+          <div className="logos">
+            <img src={USA} />
+            <img src={GMP} />
+            <img src={NonGMO} />
+          </div>
         </div>
       </div>
     </section>
