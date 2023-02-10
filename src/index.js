3194ac61331894b952e3494a2d8ae79f503893b6
Ysquid <ahk648@gmail.com>
2023-02-10 09:17:37 -0600

    GA4 added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import ReactGA from 'react-ga4';
+
+//Google Analytics initialization function
+ReactGA.initialize('G-QQLJTPLDJ6');
+
+
+//Google Analytics pageview function
+export const GAPageView = (page) => {   
+  ReactGA.pageview(page);   
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
